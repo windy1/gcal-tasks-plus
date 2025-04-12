0a7e6a847cdf3bf1fe005a982ef355ff5c970331
Walker Crouse <walker.crouse@acuitybrands.com>
2025-04-12 15:56:23 -0400

    organize files
    Signed-off-by: Walker Crouse <walker.crouse@acuitybrands.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": "/src",
+            "@components": "/src/components",
+            "@assets": "/src/assets",
+        },
+    },
 });
