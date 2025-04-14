0804ede6acda89c5053a3b8d31fd9c10ce1fa463
Walker Crouse <walker.crouse@acuitybrands.com>
2025-04-14 14:44:52 -0400

    cleanup index files
    Signed-off-by: Walker Crouse <walker.crouse@acuitybrands.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
-export * as tasks from "./tasks";
 export * from "./app";
 export * from "./button";
 export * from "./spinner";
 export * from "./task-lists";
 export * from "./tasks/tasks";
+
+export * as tasks from "./tasks";
