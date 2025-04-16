3d5f02ee923461160d97309cd2094c2d181d98d8
Walker Crouse <walkercrouse@hotmail.com>
2025-04-16 08:02:38 -0400

    docs
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- src/services/task-order-storage.ts
+++ src/services/task-order-storage.ts
@@ -1,13 +1,29 @@
 import { TaskList } from "@/data";
 
+/**
+ * Handles the storage of task order in local storage.
+ *
+ * @param taskList TaskList to store the order for
+ * @returns An object with methods to get and save the task order
+ */
 export const TaskOrderStorage = (taskList: TaskList) => {
     const storageKey = getStorageKey(taskList);
 
     return {
+        /**
+         * Retrieves the task order from local storage.
+         *
+         * @returns The task order as an array of task IDs, or null if not found
+         */
         get() {
             return localStorage.getItem(storageKey);
         },
 
+        /**
+         * Saves the task order to local storage.
+         *
+         * @param orderedIds Array of task IDs in the desired order
+         */
         save(orderedIds: string[]) {
             localStorage.setItem(storageKey, JSON.stringify(orderedIds));
         },
