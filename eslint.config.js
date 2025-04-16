38242b2bec448e9ee0ded180a85fdb0123697e81
Walker Crouse <walkercrouse@hotmail.com>
2025-04-16 08:50:15 -0400

    disallow relative imports with a depth > 2
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths";
 
 export default tseslint.config(
     { ignores: ["dist"] },
@@ -16,10 +17,15 @@ export default tseslint.config(
         plugins: {
             "react-hooks": reactHooks,
             "react-refresh": reactRefresh,
+            "no-relative-import-paths": noRelativeImportPaths,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
             "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+            "no-relative-import-paths/no-relative-import-paths": [
+                "error",
+                { allowSameFolder: true, allowedDepth: 2, rootDir: "./src" },
+            ],
         },
     },
 );
