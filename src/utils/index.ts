e149833bcdab9812b626bf33e46fc5caf62078b3
Walker Crouse <walkercrouse@hotmail.com>
2025-04-23 00:10:30 -0400

    rename utils/state -> utils/state-util
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export * as AppUtil from "./app-util";
 export * as DnD from "../utils/dnd";
-export * as StateUtil from "./state";
+export * as StateUtil from "./state-util";
 export * from "./delay";
