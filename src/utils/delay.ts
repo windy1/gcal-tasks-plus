18d4e0cdf55c60784d3f9639ed4cfb7b284da61a
Walker Crouse <walkercrouse@hotmail.com>
2025-04-23 00:28:55 -0400

    docs
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- src/utils/delay.ts
+++ src/utils/delay.ts
@@ -1,1 +1,7 @@
+/**
+ * Returns a promise that resolves after a given number of milliseconds.
+ *
+ * @param ms - The number of milliseconds to wait before resolving the promise.
+ * @returns Promise that resolves after the specified delay.
+ */
 export const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
